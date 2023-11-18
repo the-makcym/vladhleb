6c9d69a00107ebfcaf4738f6fd227f202d00344d
makcym <the.makcym@yandex.ru>
2023-11-18 14:54:48 +1000

    auth page

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,19 @@
-import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import '@/assets/main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 
-import App from './App.vue'
-import router from './router'
+import App from '@/App.vue'
+import router from '@/router'
+// import UI from '@/components/UI'
 
 const app = createApp(App)
 
-app.use(createPinia())
 app.use(router)
 
+// UI.forEach((ui) => {
+//   app.component(ui.name, ui)
+// })
+
 app.mount('#app')
+import 'bootstrap/dist/js/bootstrap.js'
